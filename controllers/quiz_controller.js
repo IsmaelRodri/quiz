1a5ba91e5269f5bfd41599b5a80a2acbeed06fa3
Ismael Rodriguez <ircueto@gmail.com>
2015-08-05 19:46:28 +0200

    modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,4 @@
+
 //GET /quizes/question
 exports.question = function (req,res) {
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
@@ -15,4 +16,25 @@ exports.answer=function(req,res){
 //GET /author
 exports.author = function (req,res) {
 	res.render('author',{respuesta: 'autor'} );
+};
+
+
+var models=require('../models/models.js');
+
+//get /quizes/question
+exports.question=function(req,res){
+	models.Quiz.findAll().then(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta})	
+})
+};
+
+//get /quizes/answer
+
+exports.answer= function(req,res){
+	models.Quiz.findAll().then(function(quiz) {
+if (req.query.respuesta===quiz[0].respuesta){
+res.render('quizes/answer',{respuesta: 'Correcto'});
+} else {res.render('quizes/answer',{respuesta: 'Incorrecto'});
+}
+})
 };
